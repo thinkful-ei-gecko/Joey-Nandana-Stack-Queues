0e272d51dc4146e2b587a3a56610b476f787cbe4
Joey Romo <joeyjr95@gmail.com>
2019-11-08 12:51:24 -0500

    1-4

--- Stacks.js
+++ Stacks.js
@@ -25,6 +25,9 @@ class Stack {
     this.top = node.next;
     return node.data;
   }
+  length() {
+    return this.top;
+  }
 }
 
 function peek(st){
@@ -39,7 +42,15 @@ function isEmpty(st){
 }
 
 function display(st){
-  return st;
+  if(st.top === null){
+    return 'this stack is empty';
+  }
+  let top = st.top;
+  while(top.next !== null){
+    console.log(top);
+    top = top.next;
+  }
+  return top.data;
 }
 
 const starTrek = new Stack();
@@ -48,6 +59,48 @@ starTrek.push('Spock');
 starTrek.push('McCoy');
 starTrek.push('Scotty');
 //console.log(peek(starTrek));
-console.log(isEmpty(starTrek));
-starTrek.pop('McCoy');
-console.log(JSON.stringify(display(starTrek)));
+// console.log(isEmpty(starTrek));
+// starTrek.pop('McCoy');
+//  console.log(display(starTrek));
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let st = new Stack();
+  let revStr = '';
+  for(let i = 0; i < s.length; i++){
+    st.push(s[i]);
+  }
+  for(let i = 0; i < s.length; i++){
+    revStr = revStr+ st.pop();
+  }
+  if(revStr === s){
+    return true;
+  }
+  return false;
+  
+  
+  
+}
+// console.log(is_palindrome('dad'));
+// console.log(is_palindrome('A man, a plan, a canal: Panama'));
+// console.log(is_palindrome('1001'));
+// console.log(is_palindrome('Tauhida'));
+function parentheses(exp){
+  let st = new Stack();
+  let check = null;
+  if(exp[0] !== '('){
+    return 'missing opening parentheses';
+  }
+  for(let i = 0; i < exp.length; i++){
+    st.push(exp[i]);
+  }
+  for(let i = 0; i < exp.length; i++) {
+    if((exp[i] === '('&& st.pop() === ')') || (exp[i] === ')' && st.pop() === '(')){
+      check = true;
+    }else{
+      return 'parentheses is missing';
+    }
+  }
+  return check;
+}
+console.log(parentheses('(((()))('));
